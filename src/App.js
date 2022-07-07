ac60aea59f65d533c669a32be93451aea368e8df
Jean-Philippe Racine <53374126+Aklar45dev@users.noreply.github.com>
2022-07-07 11:45:54 -0400

    text

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import React, { useRef, useState, useEffect } from 'react'
 
 function App() {
 
-  const chapter = [{url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-2.mp4', choice1: 'Souhaitant rattraper son erreur rapidement, dès qu’il constate son inattention, il intervient dans le groupe afin de demander des précisions.', choice2: 'Il attend que la rencontre se termine et lorsque tout le monde se dirige vers sa prochaine tâche, il interrompt le superviseur dans sa tâche pour valider les informations qu’il n’avait pas compris durant la rencontre.', choice3: 'À la sortie, il demande au superviseur quand sera le bon moment pour lui parler avant la tâche. Le superviseur lui dit qu’il passera le voir immédiatement après X. Jeff lui posera alors ces questions.', text: 'Poser des questions lorsque l’on est dans le doute est important ! Chaque situation est différente et selon celle-ci choisir le bon moment pour demander des précisions, poser des questions peut faire la différence entre une question qui est répondue de façon rapide et imprécise ou qui est répondue avec clarté. Le choix d’attendre de parler à son superviseur en privé, de demander de répéter ou d’avoir plus de précision, permet un meilleur échange sur ce qui a été compris et pas. De plus, cela favorise le dialogue et permet de réduire le stress et les appréhensions face à sa première journée de travail.'},
-                  {url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-2.mp4', choice1: 'Bien que l’expérience des collègues de travail est importante dans le milieu de travail, ce n’est pas parce leur méthode prend moins temps qu’elle est la meilleure. Au même titre que cette méthode a toujours été faite comme cela. Valider les méthodes de travail avec son superviseur est la chose à faire. ', choice2: 'Demander à son collègue qui explique en détail sa méthode de faire et la raison qui le motive à l’utiliser… Ce n’est pas parce que l’on est nouveau dans un milieu de travail que l’on ne peut pas demander plus d’explication sur une façon de faire qui de toute évidence va à l’encontre de la consigne qui est dite et écrite.', choice3: 'Ne pas écouter son collègue et faire comme s’il ne nous avait rien dit… Prendre la fiche de cadenassage et appliquer la procédure comme indiqué.', text: 'L’expérience des collègues et l’entraide sont une force dans un milieu de travail. Tous veulent être écoutés et respectés. Les ignorer lorsque leur choix et les nôtres sont contradictoires n’est pas un gage de bonne relation futur.  Cependant, prendre tout leur conseil comme de l’argent et oublier son sens critique peuvent mettre sa santé-sécurité et celle des autres en jeu. Lorsque nous sommes confrontés à ce type de situation, nous devons essayer de comprendre les motivations de nos collègues et valider avec son superviseur.'},
-                  {url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch3-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch3-2.mp4', choice1: 'Sans aviser son collègue de la situation à risque, il part rapidement aviser sont superviseur que son collègue pose une action potentiellement dangereuse.', choice2: ' Il avise son collègue que dans une situation semblable dans un autre emploi, il a vu un collègue se blesser de cette façon. Il donne beaucoup de détail sur les conséquences de l’événement et qu’il a même perdu sa femme à cause de cela.', choice3: 'Il avise son collègue du risque à utiliser une meuleuse portative sans garde de protection et sans visière. Sans donner trop de détail, il indique que par le passé, il a déjà vu un accident semblable et que même s’il ne connait pas beaucoup son nouveau collègue, il ne voudrait pas que cet événement lui arrive.', text: 'Aviser ses collègues lorsqu’ils sont dans une situation à risque est la chose à faire. Cela permet de prévenir de potentiel accident et permet de garder ses collègues au travail. Les sensibiliser sur les conséquences possibles d’un accident permet d’imaginer la situation, cependant trop donner de détail annule cet effet. Dans un milieu de travail nous sommes tous des adultes responsables, favorisé le dialogue entre les personnes permet de mieux comprendre les situations vécues de par chacun. Si l’écoute du collègue en question n’est pas présente, il faut aviser son superviseur.'}]
+  const chapter = [{url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch1-2.mp4', choice1: 'Souhaitant clarifier ses incompréhensions rapidement, il intervient immédiatement dans le groupe afin de demander des précisions.', choice2: 'À la sortie, il demande au superviseur quand sera le bon moment pour lui parler avant la tâche. Le superviseur lui dit qu’il passera le voir rapidement. Il lui posera alors ses questions.', choice3: 'Il attend que la rencontre se termine et va valider les informations avec le superviseur lorsque tout le monde se dirige vers leur tâche à accomplir.', text: 'En tant que responsable de sa propre sécurité, un travailleur ou une travailleuse qui a des interrogations ou des doutes au sujet de sa carte de travail, de sa tâche, ou autres questions pouvant affecter l’accomplissement de son travail, se doit d’interroger son supérieur afin d’obtenir des précisions. Chaque situation est différente et, choisir le bon moment pour demander des précisions ou poser des questions peut faire la différence entre une réponse qui n’est pas suffisamment précise et une réponse claireé. Faire le choix d’attendre de parler à son superviseur à la fin de la rencontre, permet un meilleur échange sur ce qui a été compris ou non. De plus, cela favorise le dialogue, permet de réduire le stress et les appréhensions face à sa première journée de travail.'},
+                  {url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch2-2.mp4', choice1: 'Incertain, il demande à son collègue qu`il détaille sa méthode et la raison qui le motive à l’utiliser, une fois les instructions reçues, il partage l’opinion de son collègue et décide donc de procéder selon cette méthode qu’ils considèrent toutes les deux comme efficace et sécuritaire.', choice2: 'Après avoir demandé des précisions à son collègue et avoir fait la lecture de la fiche de cadenassage, il choisit de faire un compromis entre les deux méthodes. Il considère que prendre le meilleur des deux solutions lui garantira un bon résultat.', choice3: 'Ne pas écouter son collègue, prendre la fiche de cadenassage et appliquer la procédure comme indiqué par le règlement.', text: 'L’expérience des collègues et l’entraide sont une force dans un milieu de travail. Tous aspirent à être écoutés et respectés. De plus, vos collègues ont certainement des choses à vous apprendre. Cependant, suivre aveuglément le conseil d’un collègue qui nous invite à ne pas respecter une règle de sécurité, ou même choisir délibérément de ne pas suivre une procédure, peut mettre la santé et la sécurité de tous en danger. Lorsque nous sommes confrontés à ce type de situation, l’important est d’exercer son sens critique et son jugement, il faut d’abord essayer de comprendre les motivations de nos collègues par la discussion, puis trouver la solution la plus sécuritaire et la valider auprès de son superviseur.'},
+                  {url: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch3-1.mp4', urlMuted: 'https://formation-video-cfpvd.s3.ca-central-1.amazonaws.com/v2/Ch3-2.mp4', choice1: 'Nouveau en poste, il ne se sent pas à l’aise d’intervenir. Sans aviser son collègue de la situation à risque, il part rapidement aviser son superviseur avant que son collègue vive une situation potentiellement dangereuse.', choice2: 'Il intervient rapidement auprès de son collègue et lui demande d’arrêter sa tâche immédiatement, il avise son collègue que la situation est dangereuse en expliquant les risques d’un tel problème.', choice3: 'Sans trop donner de détails, il avise son collègue du risque d’utiliser une meuleuse portative en lui indiquant qu’il ne voudrait pas que quelque chose de malheureux arrive.', text: 'Aviser ses collègues lorsqu’ils sont dans une situation à risque est la chose à faire. Cela permet de prévenir de potentiels accidents et permet de garder ses collègues en sécurité. Encore une fois, il est important de le faire au bon moment et surtout de la bonne façon. Les sensibiliser sur les conséquences possibles d’un accident permet d’imaginer la situation et d’en comprendre la gravité, cela donnera de la crédibilité à votre intervention. Dans un milieu de travail nous sommes tous responsables, favoriser le dialogue entre les personnes permet de mieux comprendre les situations vécues par chacun et d’éviter de futurs dangers. Si le collègue ne semble pas ouvert à la discussion, il faut en aviser le superviseur. Lui parler de votre intention de le protéger peut contribuer à convaincre la personne d’adapter son comportement.'}]
 
-  const selection = [{1: "Demander des précisions pendant le briefing.", 2: "Demander des renseignements juste après le briefing.", 3: "Demander de venir le voir rapidement pour des précisions."},
-                    {1: "Valider avec le superviseur.", 2: "Discuter pour comprendre pourquoi il ne suit pas la procédure.", 3: "Appliquer la procédure tel qu’indiqué."},
-                    {1: "Aviser son collègue et son superviseur.", 2: "Aviser son collègue et expliquer pourquoi c’est une situation à risque.", 3: "Aviser son collègue et parler de son expérience passée."}]
+  const selection = [{1: "Demander des précisions pendant la réunion préparatoire.", 2: "Demander de venir le superviseur voir rapidement pour des précisions.", 3: "Demander des renseignements au superviseur justes après la réunion préparatoire.", 4: 'Pas tout à fait...', 5: 'Bon choix!', 6: 'Pas tout à fait...'},
+                    {1: "Considérer l’expérience de son collègue et suivre ses directives même si elles sont contraires à la procédure.", 2: "Prendre le meilleur des deux options.", 3: "Appliquer la procédure indiquer dans le règlement.", 4: 'Pas la bonne réponse...', 5: 'Pas tout à fait...', 6: 'Presque!'},
+                    {1: "Aviser son superviseur sans prévenir le collègue.", 2: "Aviser son collègue et expliquer pourquoi c’est une situation à risque.", 3: "Aviser son collègue.", 4: 'Pas tout à fait...', 5: 'Presque!', 6: 'Mais encore...'}]
 
   const [url, setUrl] = useState(null)
   const [id, setId] = useState(0)
   const [ansid, setAnsid] = useState('1')
+  const [retroid, setRetroid] = useState('4')
 
   useEffect(() => {
     if(url !== null){
@@ -37,10 +38,11 @@ function App() {
     $("#selector").css({display: "flex"})
   }
 
-  const validate = (ans) => {
+  const validate = (ans, retro) => {
     $("#video").css({display: "none"})
     $("#response").css({display: "flex"})
     setAnsid(ans)
+    setRetroid(retro)
   }
 
   const retour = () => {
@@ -81,20 +83,23 @@ function App() {
         <video src={url} ref={vidRef} onEnded={showQuestions} />
         <div id="selector">
           <div className="select-title">Quel aurait été la bonne attitude à adopté?</div>
-          <button className="selectBtn" onClick={() => validate('1')}>{chapter[id].choice1}</button>
-          <button className="selectBtn" onClick={() => validate('2')}>{chapter[id].choice2}</button>
-          <button className="selectBtn" onClick={() => validate('3')}>{chapter[id].choice3}</button>
+          <button className="selectBtn" onClick={() => validate('1','4')}>{chapter[id].choice1}</button>
+          <button className="selectBtn" onClick={() => validate('2','5')}>{chapter[id].choice2}</button>
+          <button className="selectBtn" onClick={() => validate('3','6')}>{chapter[id].choice3}</button>
         </div>
       </div>
       <div id="response">
         <div className="res-container">
           <div className="info">
             <div className="info-title">{selection[id][ansid]}</div>
+            <div className="retro-subtitle">{selection[id][retroid]}</div>
             <div className="para">{chapter[id].text}</div>
           </div>
+          <div className='video-retro-container'>
             <video src={chapter[id].urlMuted} autoPlay muted loop />
+            <button className='retour' onClick={() => retour()}>Continuer</button>
+          </div>
         </div>
-        <button className='retour' onClick={() => retour()}>Continuer</button>
       </div>
     </div>
   )
